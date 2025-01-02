bde5272823d4d8a01bf0005d2c8a79ffa8fa06b5
matanbensahel <matan091296@gmail.com>
2025-01-02 18:32:27 +0200

    user controller has update function

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,24 @@
-import userModel from "../models/userModel"
+import userModel,{IUser} from "../models/userModel"
+import {Request, Response} from "express"
 import BaseController from "./baseController"
 
-const userController = new BaseController(userModel);
+class userController extends BaseController<IUser>{
+    constructor(){
+        super(userModel);
+    }
+
+    // Specific methods for this controller
+    async update(req : Request , res :Response){
+        try { 
+            const { id } = req.body.id;
+            const item = await this.model.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Item updated" });
+        } catch (error) {
+          res.status(400).json({ message: "Error fetching  ", error: error.message });
+        }
+      };
+};
 
 
 
-export default userController;
+export default new userController();
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,6 +2,8 @@ import  express from 'express';
 const router = express.Router();
 import userController from "../controllers/userController"
 
+
+
 router.get("/", userController.getAll.bind(userController))
 
 router.get("/id",userController.getById.bind(userController))
